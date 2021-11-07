0314476dd12b2a57e7d0280531b968715a3b795f
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-07 13:51:20 -0800

    About: info about TC foundation

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,37 @@
   <div id="about">
     <v-card class="about_card" outlined elevation="5">
       <v-container>
-        <h1>About Us</h1>
+        <h1 id="about_header">About Us</h1>
       </v-container>
+      <h2>The Tzu Chi Foundation</h2>
+      <p>
+        The Tzu Chi Foundation is a global relief organization whose main
+        missions are charity, education, medicine, and humanistic culture. The
+        organization was founded in Taiwan by Dharma Master Cheng in 1966, and
+        it began as a group of 30 housewives who saved money everyday, which
+        would then be donated to those in need. This incentivized others to
+        follow their acts of compassion. The organization expanded throughout
+        the years and became a major organization to provide relief services.
+        The Tzu Chi Foundation today consists of over 10 million members
+        worldwide in over 50 countries. In 2020, Tzu Chi has provided aid to 125
+        countries, donated over 2 million medical masks, provided food to over
+        70 thousand households
+      </p>
+      <p>
+        For more information about the Tzu Chi Foundation:
+        <a href="">click here to visit Tzu Chi USA's website</a>
+      </p>
     </v-card>
   </div>
 </template>
 
 <style scoped>
+#about_header {
+  text-align: center;
+  font-size: 3rem;
+  color: #2f86a6;
+}
 .about_card {
-  margin: 1%;
+  margin: 5%;
 }
 </style>
