16f26b2a82ed1ebd65e028941235bd92f89401fb
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-06 17:54:53 -0700

    home: added carousel component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
-  <hello-world />
+  <div>
+    <Carousel />
+    <hello-world />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import HelloWorld from '../components/HelloWorld';
+import Carousel from '../components/Carousel';
 
 export default {
-  name: "Home",
+  name: 'Home',
 
   components: {
     HelloWorld,
+    Carousel,
   },
 };
+Carousel;
 </script>
