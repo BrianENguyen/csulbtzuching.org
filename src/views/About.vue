8021bfc71070c4bb0736f7ba9d8464e12e0b5bcd
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-07 14:16:05 -0800

    About: "tc" meaning and applying styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,26 +3,27 @@
     <v-card class="about_card" outlined elevation="5">
       <v-container>
         <h1 id="about_header">About Us</h1>
-        <h2>The Tzu Chi Foundation</h2>
+        <h2 class="about_subheader">The Tzu Chi Foundation</h2>
         <p>
-          The Tzu Chi Foundation is a global relief organization whose main
-          missions are charity, education, medicine, and humanistic culture. The
-          organization was founded in Taiwan by Dharma Master Cheng in 1966, and
-          it began as a group of 30 housewives who saved money everyday, which
-          would then be donated to those in need. This incentivized others to
-          follow their acts of compassion. The organization expanded throughout
-          the years and became a major organization to provide relief services.
-          The Tzu Chi Foundation today consists of over 10 million members
-          worldwide in over 50 countries. In 2020, Tzu Chi has provided aid to
-          125 countries, donated over 2 million medical masks, provided food to
-          over 70 thousand households, and benefitted 17,335 students from their
+          The Tzu Chi Foundation (<em>tzu</em> - "compassionate", <em>chi</em> -
+          "relief") is a global relief organization whose main missions are
+          charity, education, medicine, and humanistic culture. The organization
+          was founded in Taiwan by Dharma Master Cheng in 1966, and it began as
+          a group of 30 housewives who saved money everyday, which would then be
+          donated to those in need. This incentivized others to follow their
+          acts of compassion. The organization expanded throughout the years and
+          became a major organization to provide relief services. The Tzu Chi
+          Foundation today consists of over 10 million members worldwide in over
+          50 countries. In 2020, Tzu Chi has provided aid to 125 countries,
+          donated over 2 million medical masks, provided food to over 70
+          thousand households, and benefitted 17,335 students from their
           tutoring program
         </p>
         <p>
           For more information about the Tzu Chi Foundation:
           <a href="">click here to visit Tzu Chi USA's website</a>
         </p>
-        <h2>CSULB Tzu Ching</h2>
+        <h2 class="about_subheader">CSULB Tzu Ching</h2>
         <p>
           The Tzu Chi Foundation consists of collegiate chapters throughout the
           world. CSULB Tzu Ching (<em>tzu</em> - "compassionate",
@@ -43,7 +44,10 @@
   font-size: 3rem;
   color: #2f86a6;
 }
+.about_subheader {
+  color: #3babd4;
+}
 .about_card {
-  margin: 5%;
+  margin: 5% 15%;
 }
 </style>
