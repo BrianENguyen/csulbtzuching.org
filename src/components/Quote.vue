e323b56f520c93b5afe1476b5227148fe15a92c2
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-07 11:13:01 -0800

    Quote: starting quote

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <v-container>
+    <h3>Be grateful for the past</h3>
+    <h3>Look forward to the future</h3>
+    <h3>Take advantage of the present</h3>
+    <p><em>- Dharma Master Cheng Yen</em></p>
+  </v-container>
 </template>
 
 <script>
