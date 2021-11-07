a68cbac7bb1dfc5b447ea2772349bd6e8944b427
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-07 11:41:48 -0800

    Quote: styles for quote text

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <h3>Be grateful for the past</h3>
-    <h3>Look forward to the future</h3>
-    <h3>Take advantage of the present</h3>
+    <div class="quote_content">
+      <h2 class="quote_1">Be grateful for the past</h2>
+      <h2 class="quote_2">Look forward to the future</h2>
+      <h2 class="quote_3">Take advantage of the present</h2>
+    </div>
     <p><em>- Dharma Master Cheng Yen</em></p>
   </div>
 </template>
@@ -11,4 +13,20 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+div.quote_content {
+  font-size: 1.5rem;
+}
+.quote_1 {
+  color: #2f86a6;
+  font-weight: 300;
+}
+.quote_2 {
+  color: #34be82;
+  font-weight: 300;
+}
+.quote_3 {
+  color: #2fdd92;
+  font-weight: 300;
+}
+</style>
