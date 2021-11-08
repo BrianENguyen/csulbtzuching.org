67254d6ddb08d9db3bf786b4572b7dde4e54c722
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-07 16:39:10 -0800

    Officers: added basic structure

--- src/views/Officers.vue
+++ src/views/Officers.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Officers</h1>
+  <div>
+    <v-container>
+      <h1>Officers</h1>
+      <v-row>
+        <v-col cols="6">
+          <OfficerCard />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
+<script>
+import OfficerCard from '../components/OfficerCard';
+export default {
+  components: {
+    OfficerCard,
+  },
+};
+</script>
