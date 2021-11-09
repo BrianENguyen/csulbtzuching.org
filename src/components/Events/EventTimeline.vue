d6a432e7151844b5e35c9c802c25857ff8812be4
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-09 11:38:03 -0800

    EventTimeline: basic structure

--- src/components/Events/EventTimeline.vue
+++ src/components/Events/EventTimeline.vue
@@ -1,5 +1,18 @@
 <template>
-  <v-timeline>
-    <v-timeline-item>Test</v-timeline-item>
-  </v-timeline>
+  <v-container>
+    <v-timeline :reverse="reverse" dense>
+      <v-timeline-item>
+        <EventCard />
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
+
+<script>
+import EventCard from './EventCard.vue';
+export default {
+  components: {
+    EventCard,
+  },
+};
+</script>
