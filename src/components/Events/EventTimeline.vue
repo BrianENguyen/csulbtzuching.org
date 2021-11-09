918750007484c8bf1173e18d494bd09faddcd78d
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-09 12:55:33 -0800

    code from EventTimeline to EventTimelineItem

--- src/components/Events/EventTimeline.vue
+++ src/components/Events/EventTimeline.vue
@@ -1,30 +1,24 @@
 <template>
   <v-timeline dense>
-    <v-timeline-item v-for="(event, i) in events" :key="i">
-      <v-row>
-        <v-col cols="7">
-          <EventCard :event="event" />
-        </v-col>
-        <v-col cols="5">
-          <h2 class="text-center">{{ event.date }} @ {{ event.time }}</h2>
-        </v-col>
-      </v-row>
-    </v-timeline-item>
+    <div v-for="(event, i) in events" :key="i">
+      <EventTimelineItem :event="event" />
+    </div>
   </v-timeline>
 </template>
 
 <script>
-import EventCard from './EventCard.vue';
+import EventTimelineItem from './EventTimelineItem.vue';
+
 export default {
   components: {
-    EventCard,
+    EventTimelineItem,
   },
   data: () => ({
     events: [
       {
         name: 'Medical Outreach',
         date: 'Saturday, 11/13/21',
-        time: '9:00 AM',
+        time: '9:10 AM',
         location: '123 Sesame Street',
         description: 'This is a test',
         img: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_678/v1636487294/CSULB_TC_Website/DSC_0118_c3fvq6.jpg',
---
+++ src/components/Events/EventTimelineItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-timeline-item>
+    <v-row>
+      <v-col cols="7">
+        <EventCard :event="event" />
+      </v-col>
+      <v-col cols="5">
+        <h2 class="text-center">{{ event.date }} @ {{ event.time }}</h2>
+      </v-col>
+    </v-row>
+  </v-timeline-item>
+</template>
+
+<script>
+import EventCard from './EventCard';
+export default {
+  props: {
+    event: Object,
+  },
+  components: {
+    EventCard,
+  },
+};
+</script>
