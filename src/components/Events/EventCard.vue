3a8f2d532f959481155182a5d6acb4198aa02a71
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-09 11:53:09 -0800

    EventsCard: basic format

---
+++ src/components/Events/EventCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card max-width="500">
+    <v-img
+      height="200px"
+      src="https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_678/v1636487294/CSULB_TC_Website/DSC_0118_c3fvq6.jpg"
+    ></v-img>
+    <v-card-title>Test</v-card-title>
+    <v-card-text
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta animi
+      optio minus beatae in fuga, possimus modi? Necessitatibus distinctio
+      impedit asperiores, natus amet optio, saepe autem enim quae magni
+      nam?</v-card-text
+    >
+    <v-card-actions>
+      <v-btn color="blue" text>Sign Up</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
