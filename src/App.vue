347fb7ae3e19b300dab91991084a6fac4ea066b5
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-11 13:09:44 -0800

    App: removed temp router

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,6 @@
   <v-app>
     <Navbar />
     <v-main>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/team">Team</router-link> |
-        <router-link to="/events">Events</router-link> |
-        <router-link to="/photos">Photos</router-link> |
-        <router-link to="/contact">Contact</router-link>
-      </div>
       <router-view />
     </v-main>
   </v-app>
