a930058a8b6fdb12c1df8cc399c5f63ceafcaa7d
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-11 11:39:33 -0800

    EventCard: style changes

--- src/components/Events/EventCard.vue
+++ src/components/Events/EventCard.vue
@@ -1,10 +1,9 @@
 <template>
   <v-card max-width="500" elevation="5">
-    <v-img height="200px" :src="event.img"></v-img>
-    <v-card-title>{{ event.name }}</v-card-title>
+    <v-img height="150px" :src="event.img"></v-img>
+    <v-card-title class="event_name">{{ event.name }}</v-card-title>
     <v-card-subtitle>
-      {{ event.date }} @ {{ event.time }}
-      <br />
+      <h4>{{ event.date }} @ {{ event.time }}</h4>
       {{ event.location }}
     </v-card-subtitle>
     <v-card-text>{{ event.description }}</v-card-text>
@@ -27,5 +26,12 @@ export default {
 <style scoped>
 .signup_link {
   text-decoration: none;
+}
+.event_name {
+  color: #34be82;
+}
+.event_description {
+  font-weight: 300;
+  color: black;
 }
 </style>
