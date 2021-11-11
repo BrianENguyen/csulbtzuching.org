e7da2d43ec95aed73d5d864636f23e263305583f
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-11 13:01:48 -0800

    import bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 new Vue({
   router,
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
