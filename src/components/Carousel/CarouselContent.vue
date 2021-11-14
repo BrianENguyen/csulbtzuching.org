63c8adb29ca520bf411e926ae1cf18db4838854f
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-14 11:26:09 -0800

    init CarouselContent.vue

---
+++ src/components/Carousel/CarouselContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="carousel_item">
+    <h1 class="carousel_header">{{ section.header }}</h1>
+    <p class="carousel_description">{{ section.description }}</p>
+    <div v-if="section.link">
+      <router-link :to="section.link" class="carousel_btn"
+        ><v-btn>{{ section.button }}</v-btn>
+      </router-link>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
