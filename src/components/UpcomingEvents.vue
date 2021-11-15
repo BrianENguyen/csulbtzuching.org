31e1021d3a0a996c054e4c99307a631ff6b78575
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-14 22:12:51 -0800

    UpcomingEvents: grid layout

--- src/components/UpcomingEvents.vue
+++ src/components/UpcomingEvents.vue
@@ -2,9 +2,11 @@
   <v-container id="recent_events_section">
     <h1 id="recent_events_header">Upcoming Events</h1>
     <h2 class="recent_events_month">{{ month }}</h2>
-    <div v-for="event in events" :key="event.id" class="recent_event">
-      <UpcomingEventCard :event="event" />
-    </div>
+    <v-row>
+      <v-col v-for="(event, i) in events" :key="i" cols="12" md="6">
+        <UpcomingEventCard :event="event" />
+      </v-col>
+    </v-row>
     <hr />
   </v-container>
 </template>
@@ -19,14 +21,12 @@ export default {
     month: 'November 2021',
     events: [
       {
-        id: 1,
         name: 'Medical Outreach',
         date: 'Saturday 11/6/21',
         description:
           'Help medical professionals provide services to those in need',
       },
       {
-        id: 2,
         name: "Weili's Warm Gathering",
         date: 'Wednesday 11/3/21',
         description:
