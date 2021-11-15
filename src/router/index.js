cf890818883730676a0a018620ec2abf2980e748
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-14 19:09:14 -0800

    router: added meta tags for each view

--- src/router/index.js
+++ src/router/index.js
@@ -14,31 +14,37 @@ const routes = [
     path: '/',
     name: 'Home',
     component: HomeView,
+    meta: { title: 'CSULB Tzu Ching' },
   },
   {
     path: '/about',
     name: 'About',
     component: AboutView,
+    meta: { title: 'About | CSULB TC' },
   },
   {
     path: '/team',
     name: 'Team',
     component: TeamView,
+    meta: { title: 'Team | CSULB TC' },
   },
   {
     path: '/events',
     name: 'Events',
     component: EventsView,
+    meta: { title: 'Events | CSULB TC' },
   },
   {
     path: '/photos',
     name: 'Photos',
     component: PhotosView,
+    meta: { title: 'Photos | CSULB TC' },
   },
   {
     path: '/contact',
     name: 'Contact',
     component: ContactView,
+    meta: { title: 'Contact | CSULB TC' },
   },
 ];
 
