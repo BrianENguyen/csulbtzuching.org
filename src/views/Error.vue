d57435fefd1ce47a272c6249a3b35c578f033580
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-14 19:16:38 -0800

    init Error.vue

---
+++ src/views/Error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>404 Error</h1>
+    <h3>The page you are on doesn't or no longer exists</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
