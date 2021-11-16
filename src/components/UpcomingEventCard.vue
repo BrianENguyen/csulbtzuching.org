b06d78b14f65479be543e608ad4a337fdcbf7ed9
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-16 11:09:01 -0800

    UpcomingEventCard: style changes

--- src/components/UpcomingEventCard.vue
+++ src/components/UpcomingEventCard.vue
@@ -1,9 +1,21 @@
 <template>
-  <v-card
-    ><h3 class="recent_event_name">{{ event.name }}</h3>
-    <h4 class="recent_event_date">{{ event.date }}</h4>
-    <p>{{ event.description }}</p>
-    <v-btn>Sign Up</v-btn>
+  <v-card elevation="3">
+    <v-card-title>
+      <h1 class="upcoming_event_name">
+        {{ event.name }}
+      </h1>
+    </v-card-title>
+    <v-card-subtitle>
+      <h4 class="upcoming_event_date">{{ event.date }}</h4>
+    </v-card-subtitle>
+    <v-card-text>
+      <p class="upcoming_event_description">
+        {{ event.description }}
+      </p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="blue" text>Sign Up</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -16,14 +28,14 @@ export default {
 </script>
 
 <style>
-.recent_events_month {
-  color: #3babd4;
-}
-
-.recent_event_name {
+.upcoming_event_name {
+  font-size: 1.5rem;
   color: #34be82;
 }
-.recent_event_date {
-  color: #34be82;
+.upcoming_event_date {
+  font-size: 1rem;
+}
+.upcoming_event_description {
+  font-weight: 300;
 }
 </style>
