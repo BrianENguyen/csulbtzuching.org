783af56e32665b95f9f1bd55c917f00c3eb5e632
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-17 13:05:46 -0800

    RecentPhotos: v-for for rendering photos

--- src/components/RecentPhotos.vue
+++ src/components/RecentPhotos.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>
-    <v-row>
-      <v-col> <PhotoCard /></v-col
-    ></v-row>
-  </div>
+  <v-row class="fill-height">
+    <v-col v-for="(photo, i) in photos" :key="i" cols="12" md="6" lg="4">
+      <PhotoCard /> </v-col
+  ></v-row>
 </template>
 
 <script>
@@ -14,6 +13,14 @@ export default {
   components: {
     PhotoCard,
   },
+  data: () => ({
+    photos: [
+      'https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg',
+    ],
+  }),
 };
 </script>
 
