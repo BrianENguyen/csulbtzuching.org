64d26fb6225540ff6a780f5fc88908924199b48f
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-17 12:13:33 -0800

    ImageCard: hover cursor on popup

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,18 +1,11 @@
 <template>
   <v-dialog v-model="dialog" width="1000">
-    <!-- <v-img
-      src="https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg"
-      aspect-ratio="1"
-      width="300px"
-      class="image-card"
-    >
-    </v-img> -->
     <template v-slot:activator="{ on, attrs }">
       <v-img
         src="https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg"
         aspect-ratio="1"
         width="300px"
-        class="image-card"
+        class="image_card"
         v-bind="attrs"
         v-on="on"
       >
@@ -25,6 +18,7 @@
         width="1000px"
         v-bind="attrs"
         v-on="on"
+        class="image_card_popup"
       >
       </v-img>
     </center>
@@ -36,19 +30,22 @@ export default {
   data: () => ({
     dialog: false,
   }),
-  components: {},
 };
 </script>
 
 <style>
-.image-card {
+.image_card {
   transition: 0.5s ease;
   border-radius: 3%;
   border-style: solid;
   border-color: #e2e2e2;
 }
-.image-card:hover {
+.image_card:hover {
   transition: 0.5s ease;
   box-shadow: rgba(0, 0, 0, 0.55) 0px 5px 15px;
+  cursor: pointer;
+}
+.image_card_popup:hover {
+  cursor: pointer;
 }
 </style>
