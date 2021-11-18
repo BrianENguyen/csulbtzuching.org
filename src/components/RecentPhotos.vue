b58f21d12597263a9d002ad4b431193cfaa7f562
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-18 11:48:09 -0800

    RecentPhotos: various changes
    - added new photos
    - header for section
    - button that links to Photos page

--- src/components/RecentPhotos.vue
+++ src/components/RecentPhotos.vue
@@ -1,8 +1,16 @@
 <template>
-  <v-row class="fill-height">
-    <v-col v-for="(photo, i) in photos" :key="i" cols="12" md="6" lg="4">
-      <PhotoCard :photo="photo" /> </v-col
-  ></v-row>
+  <div>
+    <h1 id="recent_photos_header">Recent Photos</h1>
+    <v-row class="fill-height">
+      <v-col v-for="(photo, i) in photos" :key="i" cols="12" md="6" lg="4">
+        <PhotoCard :photo="photo" /> </v-col
+    ></v-row>
+    <center>
+      <router-link to="/photos" class="photos_btn_hover">
+        <v-btn x-large class="photos_btn">View More Photos</v-btn>
+      </router-link>
+    </center>
+  </div>
 </template>
 
 <script>
@@ -15,13 +23,22 @@ export default {
   },
   data: () => ({
     photos: [
-      'https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/v1636487294/CSULB_TC_Website/DSC_0118_c3fvq6.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/v1636913788/CSULB_TC_Website/17546836_656892331149578_6206756973583337194_o_f82awv.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1637183817/CSULB_TC_Website/6F3E6219-7561-4D01-AA4A-B13A735CCE80_kjncp4.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1637183822/CSULB_TC_Website/IMG_9996_bdgkjf.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1637183820/CSULB_TC_Website/IMG_0040_txeaj1.jpg',
     ],
   }),
 };
 </script>
 
-<style></style>
+<style>
+#recent_photos_header {
+  color: #2f86a6;
+}
+.photos_btn {
+  margin: 4rem 0px;
+}
+.photos_btn_hover:hover {
+  text-decoration: none;
+}
+</style>
