90e29913f949ef0f8ad7e1c24d3c8eb7b6b31786
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-18 12:38:12 -0800

    PhotoGallery: test data

--- src/components/PhotoGallery.vue
+++ src/components/PhotoGallery.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <h2>{{ event.name }}</h2>
+    <v-row>
+      <v-col v-for="photo in event.photos" :key="photo" cols="12" md="6" lg="4">
+        <PhotoCard :photo="photo" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {};
+import PhotoCard from './PhotoCard.vue';
+export default {
+  components: {
+    PhotoCard,
+  },
+  props: {
+    event: Object,
+  },
+};
 </script>
 
 <style></style>
--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Photos</h1>
+  <v-container>
+    <h1>Photos</h1>
+    <div v-for="(event, i) in events" :key="i">
+      <PhotoGallery :event="event" />
+    </div>
+  </v-container>
 </template>
+
+<script>
+import PhotoGallery from '../components/PhotoGallery.vue';
+export default {
+  components: {
+    PhotoGallery,
+  },
+  data: () => ({
+    events: [
+      {
+        name: 'Medical Outreach',
+        date: '11/13/21',
+        photos: [
+          'https://res.cloudinary.com/buraiyen/image/upload/v1637183817/CSULB_TC_Website/6F3E6219-7561-4D01-AA4A-B13A735CCE80_kjncp4.jpg',
+        ],
+      },
+    ],
+  }),
+};
+</script>
+
+<style></style>
