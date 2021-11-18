4b1e01eeeb4bd1f0d75249b9ba94e2aa3635eacb
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-18 13:08:36 -0800

    PhotosView: more data

--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <h1>Photos</h1>
+    <h3>Get a glimpse at what we do in our organization!</h3>
     <div v-for="(event, i) in events" :key="i">
       <PhotoGallery :event="event" />
     </div>
@@ -20,6 +21,19 @@ export default {
         date: '11/13/21',
         photos: [
           'https://res.cloudinary.com/buraiyen/image/upload/v1637183817/CSULB_TC_Website/6F3E6219-7561-4D01-AA4A-B13A735CCE80_kjncp4.jpg',
+          'https://res.cloudinary.com/buraiyen/image/upload/v1637183822/CSULB_TC_Website/IMG_9996_bdgkjf.jpg',
+          'https://res.cloudinary.com/buraiyen/image/upload/v1637183820/CSULB_TC_Website/IMG_0040_txeaj1.jpg',
+        ],
+      },
+      {
+        name: 'Fall Week of Welcome',
+        date: '9/5/21',
+        photos: [
+          'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1637269283/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1548_pynqen.jpg',
+          'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1084/v1637269278/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1632_sdahq2.jpg',
+          'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1148/v1637269268/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1689_fjaew9.jpg',
+          'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_892/v1637269276/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1533_uplruz.jpg',
+          'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1020/v1637269282/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1661_klychz.jpg',
         ],
       },
     ],
