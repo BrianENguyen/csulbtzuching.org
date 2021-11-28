e921c2971fd280c0bc9b2d74175d5cae6f9e354a
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-27 19:36:15 -0800

    FAQ: removed data section; imbed text instead

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,33 +1,87 @@
 <template>
   <div>
     <h2 class="about_subheader">Frequently Asked Questions</h2>
-
     <v-expansion-panels>
-      <v-expansion-panel
-        v-for="(questionAnswer, i) in questionAnswers"
-        :key="i"
-      >
-        <v-expansion-panel-header
-          ><h4>{{ questionAnswer.question }}</h4>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h4>1. Do I have to pay dues to become a member?</h4>
         </v-expansion-panel-header>
         <v-expansion-panel-content>
-          {{ questionAnswer.answer }}
+          <p>
+            No! You do not have to pay a single penny to be part of our
+            organization
+          </p>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h4>2. Do I have to be Buddhist or Asian?</h4>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          No! The Tzu Chi Foundation is an international organization that has
+          volunteers from many different backgrounds, races, religions, etc.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h4>3. Do I have to be vegetarian / vegan?</h4>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <p>
+            We promote environmental protection by having a vegan / vegetarian
+            diet. This diet eliminates environmental pollution and shows
+            compassion towards animals; however, you do
+            <strong>NOT</strong> have to be vegetarian / vegan to be part of our
+            organization
+          </p>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h4>4. What's the difference between "Tzu Chi" and "Tzu Ching"?</h4>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <p>
+            Tzu Chi is our main international-relief organization. In Chinese,
+            it means "Compassionate Relief"
+          </p>
+          <p>
+            Tzu Ching is the collegiate-level of Tzu Chi. In Chinese, it means
+            "Compassionate Youth." We are the CSULB collegiate branch of Tzu
+            Chi, hence we are called CSULB Tzu Ching. We are also sometimes
+            called the Tzu Chi Collegiate Association of CSULB. Hopefully those
+            terms aren't confusing!!
+          </p>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h4>5. What is the uniform that you wear? How can I get one?</h4>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <p>
+            Tzu Chi members wear a blue Tzu Chi shirt, white pants, and white
+            socks for their uniform. It signifies blue skies and white clouds.
+            By wearing our uniforms, we are building a presence for those who
+            are receiving our help
+          </p>
+          <p>
+            To obtain a uniform, you must reach 20 hours of volunteering across
+            4 unique events. This includes food distributions, medical
+            outreaches, etc. We also have virtual volunteering for you at
+            beanbeanbean.com and freerice.com, as well as sharing us your veggie
+            foods. General meetings, book club gatherings, and Dharma studies
+            also count towards your hours. Social events DO NOT count. Hours
+            carry over between semesters / years
+          </p>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
   </div>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    questionAnswers: [
-      {
-        question: '1. Do I have to pay dues to become a member?',
-        answer:
-          'No! You do not have to pay a single penny to be part of our organization',
-      },
-    ],
-  }),
-};
-</script>
+<style scoped>
+.answer {
+  margin: 0px 20px;
+}
+</style>
