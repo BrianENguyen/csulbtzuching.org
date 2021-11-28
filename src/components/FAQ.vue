8a96fad292aa51c37a69709e24440a067988977e
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-27 19:01:25 -0800

    FAQ: basic structure

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,7 +1,33 @@
-<template></template>
+<template>
+  <div>
+    <h2 class="about_subheader">Frequently Asked Questions</h2>
+
+    <v-expansion-panels>
+      <v-expansion-panel
+        v-for="(questionAnswer, i) in questionAnswers"
+        :key="i"
+      >
+        <v-expansion-panel-header
+          ><h4>{{ questionAnswer.question }}</h4>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          {{ questionAnswer.answer }}
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    questionAnswers: [
+      {
+        question: '1. Do I have to pay dues to become a member?',
+        answer:
+          'No! You do not have to pay a single penny to be part of our organization',
+      },
+    ],
+  }),
+};
 </script>
-
-<style></style>
