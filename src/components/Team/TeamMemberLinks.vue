fd332f48d8ee88dca7b3bf5fe8e066ab130153fd
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-27 18:03:28 -0800

    TeamMemberLinks: reordered links

--- src/components/Team/TeamMemberLinks.vue
+++ src/components/Team/TeamMemberLinks.vue
@@ -3,14 +3,14 @@
     <a v-if="links.email" :href="links.email" target="_blank"
       ><i class="fas fa-envelope fa-2x mr-2 member_card_icon"></i
     ></a>
-    <a v-if="links.facebook" :href="links.facebook" target="_blank">
-      <i class="fab fa-facebook fa-2x mx-2 member_card_icon"></i
+    <a v-if="links.site" :href="links.site" target="_blank">
+      <i class="fas fa-link fa-2x mx-2 member_card_icon"></i
     ></a>
     <a v-if="links.linkedin" :href="links.linkedin" target="_blank">
       <i class="fab fa-linkedin fa-2x mx-2 member_card_icon"></i
     ></a>
-    <a v-if="links.site" :href="links.site" target="_blank">
-      <i class="fas fa-link fa-2x mx-2 member_card_icon"></i
+    <a v-if="links.facebook" :href="links.facebook" target="_blank">
+      <i class="fab fa-facebook fa-2x mx-2 member_card_icon"></i
     ></a>
     <a v-if="links.instagram" :href="links.instagram" target="_blank">
       <i class="fab fa-instagram fa-2x mx-2 member_card_icon"></i
