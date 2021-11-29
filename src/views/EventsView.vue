13bec0b24add726216556fa3cf271ba9e4a87c5e
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-29 15:39:21 -0800

    Events: actual data

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -6,9 +6,11 @@
       :label="`Condensed view: ${isCondensed.toString()}`"
     ></v-checkbox>
     <div v-if="isCondensed">
+      <EventTimelineCondensed month="December 2021" :events="december" />
       <EventTimelineCondensed month="November 2021" :events="november" />
     </div>
     <div v-else>
+      <EventTimeline month="December 2021" :events="december" />
       <EventTimeline month="November 2021" :events="november" />
     </div>
   </CardWrapper>
@@ -29,7 +31,23 @@ export default {
   },
   data: () => ({
     isCondensed: false,
+    december: [
+      {
+        name: 'December General Meeting',
+        date: 'Date and time TBA',
+        time: '',
+        location: 'Zoom',
+        description: '',
+      },
+    ],
     november: [
+      {
+        name: 'Bowling Social',
+        date: 'Sunday, 11/21/21',
+        time: '12:00 PM - 2:00PM',
+        location: '1355 Broad Ave, Wilmington, CA 90744',
+        description: '',
+      },
       {
         name: 'Medical Outreach',
         date: 'Saturday, 11/13/21',
@@ -38,7 +56,7 @@ export default {
         description:
           'Assist medical professionals in providing medical services to those in need!',
         img: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_678/v1636487294/CSULB_TC_Website/DSC_0118_c3fvq6.jpg',
-        link: 'TBA',
+        link: '',
       },
     ],
   }),
