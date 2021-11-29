9db2ffe090a8922aa3f369f28d95f696b5122ef4
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-28 18:41:31 -0800

    PageHeader: basic structure

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1 class="header">
+    <slot></slot>
+  </h1>
+</template>
+
+<style>
+@media (min-width: 0px) {
+  .header {
+    font-size: 5.25vw;
+  }
+}
+
+@media (min-width: 576px) {
+  .header {
+    font-size: 4.5vw;
+  }
+}
+@media (min-width: 992px) {
+  .header {
+    font-size: 4vw;
+  }
+}
+
+@media (min-width: 1200px) {
+  .header {
+    font-size: 3vw;
+  }
+}
+</style>
