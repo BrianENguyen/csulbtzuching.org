001de8c17e3e5091d61f28da0bb78c07929b8099
buraiyen <brian.edison.nguyen@gmail.com>
2021-11-28 19:05:20 -0800

    PageTitle: basic structure

---
+++ src/components/UI/Header/PageTitle.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 class="page_title">
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.page_title {
+  font-size: 3vw;
+  color: #2f86a6;
+}
+@media (min-width: 0px) {
+  .page_title {
+    font-size: 5.25vw;
+  }
+}
+
+@media (min-width: 576px) {
+  .page_title {
+    font-size: 4.5vw;
+  }
+}
+@media (min-width: 992px) {
+  .page_title {
+    font-size: 4vw;
+  }
+}
+
+@media (min-width: 1200px) {
+  .page_title {
+    font-size: 3vw;
+  }
+}
+</style>
