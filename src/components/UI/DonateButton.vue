3b27e65dfb941cb8a7daed4187318ce6c572cd6e
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-01 15:48:02 -0800

    DonateButton: styles

---
+++ src/components/UI/DonateButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <a class="donation_button" :href="info.link" target="_blank">
+    <v-btn rounded :color="info.color" dark large>
+      <!-- <v-img :src="img" width="25px"></v-img> -->
+      <v-img :src="info.logo" width="25px"></v-img>
+      Donate
+    </v-btn>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    info: Object,
+  },
+};
+</script>
+
+<style scoped>
+.donation_button:hover {
+  text-decoration: none;
+}
+</style>
