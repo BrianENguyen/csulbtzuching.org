b6c54f7ae3915f816373e8b43212d4a9799da876
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-01 13:58:16 -0800

    DonateView: basic structure and router

--- src/views/DonateView.vue
+++ src/views/DonateView.vue
@@ -1,7 +1,29 @@
-<template></template>
+<template>
+  <CardWrapper>
+    <PageTitle>Donate</PageTitle>
+    <p>
+      Donations are greatly appreciated because they help keep the website
+      online! If you want to support our organization, then consider donating!
+    </p>
+    <p>
+      If you wish to donate to the Tzu Chi Foundation, please
+      <a href="https://donate.tzuchi.us/cause/gift/">click here.</a> Your
+      donation will help Tzu Chi with their efforts in providing relief, like
+      food, clothing, and medical care!
+    </p>
+  </CardWrapper>
+</template>
 
 <script>
-export default {};
+import CardWrapper from '../components/UI/CardWrapper';
+import PageTitle from '../components/UI/Header/PageTitle';
+
+export default {
+  components: {
+    CardWrapper,
+    PageTitle,
+  },
+};
 </script>
 
 <style></style>
