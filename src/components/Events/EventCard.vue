59c4f9166ce7040795ab4d4a7985220aeae9c5b0
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-01 17:47:37 -0800

    EventCard: added lazy src and placeholder

--- src/components/Events/EventCard.vue
+++ src/components/Events/EventCard.vue
@@ -1,6 +1,20 @@
 <template>
   <v-card max-width="400" elevation="5">
-    <v-img v-if="event.img" height="200px" :src="event.img" />
+    <v-img
+      v-if="event.img"
+      height="200px"
+      :src="event.img"
+      :lazy-src="event.img"
+    >
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular
+            indeterminate
+            color="grey lighten-5"
+          ></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
     <v-card-title class="event_name">{{ event.name }}</v-card-title>
     <v-card-subtitle>
       <h4 v-if="event.date && event.time" class="event_datetime">
