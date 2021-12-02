11886be79ba33b572a330ef052379f74009ef663
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-01 16:00:17 -0800

    removed comments

--- src/components/UI/DonateButton.vue
+++ src/components/UI/DonateButton.vue
@@ -1,8 +1,7 @@
 <template>
   <a class="donation_button" :href="info.link" target="_blank">
     <v-btn rounded :color="info.color" dark large>
-      <!-- <v-img :src="img" width="25px"></v-img> -->
-      <v-img :src="info.logo" width="25px"></v-img>
+      <v-img :src="info.logo" width="30px"></v-img>
       Donate
     </v-btn>
   </a>
