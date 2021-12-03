4c7d2469a0d2a0bb2e3ae7b8a8bcb0e9d5adb872
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-03 15:26:49 -0800

    Quote: changed colors

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <div class="quote_content">
-      <h2 class="quote_1">"Be grateful for the past</h2>
-      <h2 class="quote_2">Look forward to the future</h2>
-      <h2 class="quote_3">Take advantage of the present"</h2>
+      <div class="quote">
+        <h2>"Be grateful for the past</h2>
+        <h2>Look forward to the future</h2>
+        <h2>Take advantage of the present"</h2>
+      </div>
       <footer id="author">— <cite>Dharma Master Cheng Yen</cite></footer>
     </div>
   </div>
@@ -14,20 +16,11 @@
   color: #949494;
   font-size: 1.5rem;
 }
+.quote {
+  color: #505050;
+}
 div.quote_content {
   font-size: 1.5rem;
   text-align: center;
-}
-.quote_1 {
-  color: #2f86a6;
-  font-weight: 300;
-}
-.quote_2 {
-  color: #34be82;
-  font-weight: 300;
-}
-.quote_3 {
-  color: #2fdd92;
-  font-weight: 300;
 }
 </style>
