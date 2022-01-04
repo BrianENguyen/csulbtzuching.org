00d4b3e501c236521e78c53416ce7e8f14141533
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-04 13:51:25 -0800

    TeamView: jumbo data

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,45 +1,54 @@
 <template>
-  <CardWrapper>
-    <PageTitle>2021-2022 Team</PageTitle>
-    <div class="section_spacer">
-      <PageHeader class="text-center">Officers</PageHeader>
-      <v-row>
-        <v-col
-          cols="12"
-          lg="4"
-          md="6"
-          sm="12"
-          v-for="(officer, i) in officers"
-          :key="i"
-        >
-          <TeamMemberCard :member="officer" />
-        </v-col>
-      </v-row>
-    </div>
-    <div>
-      <PageHeader class="text-center">Alumni</PageHeader>
-      <v-row>
-        <v-col
-          cols="12"
-          lg="4"
-          md="6"
-          sm="12"
-          v-for="(alumni, i) in alumnis"
-          :key="i"
-        >
-          <TeamMemberCard :member="alumni" />
-        </v-col>
-      </v-row>
-    </div>
-  </CardWrapper>
+  <div class="team">
+    <Jumbotron :jumboData="jumboData" />
+    <CardWrapper>
+      <PageTitle>2021-2022 Team</PageTitle>
+      <div class="section_spacer">
+        <PageHeader class="text-center">Officers</PageHeader>
+        <v-row>
+          <v-col
+            cols="12"
+            lg="4"
+            md="6"
+            sm="12"
+            v-for="(officer, i) in officers"
+            :key="i"
+          >
+            <TeamMemberCard :member="officer" />
+          </v-col>
+        </v-row>
+      </div>
+      <div>
+        <PageHeader class="text-center">Alumni</PageHeader>
+        <v-row>
+          <v-col
+            cols="12"
+            lg="4"
+            md="6"
+            sm="12"
+            v-for="(alumni, i) in alumnis"
+            :key="i"
+          >
+            <TeamMemberCard :member="alumni" />
+          </v-col>
+        </v-row>
+      </div>
+    </CardWrapper>
+  </div>
 </template>
 
 <script>
+import Jumbotron from '../components/UI/Jumbotron';
 import TeamMemberCard from '../components/Team/TeamMemberCard';
 import CardWrapper from '../components/UI/CardWrapper';
 import PageTitle from '../components/UI/Header/PageTitle';
 import PageHeader from '../components/UI/Header/PageHeader';
 import { officers, alumnis } from '../components/Helpers/TeamData';
+
+const jumboImage =
+  'https://res.cloudinary.com/buraiyen/image/upload/v1636919257/CSULB_TC_Website/BEN_1689_01_hobk1j.jpg';
+const jumboTitle = 'Our Board Members';
+const jumboDescription = 'Meet our current officers and alumni!';
 
 export default {
   components: {
@@ -47,10 +56,16 @@ export default {
     CardWrapper,
     PageTitle,
     PageHeader,
+    Jumbotron,
   },
   data: () => ({
     officers: officers,
     alumnis: alumnis,
+    jumboData: {
+      src: jumboImage,
+      title: jumboTitle,
+      description: jumboDescription,
+    },
   }),
 };
 </script>
