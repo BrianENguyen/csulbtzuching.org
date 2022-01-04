01617d70ca7fa9fc42b1e22274eb4e0b336744d3
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-04 13:09:51 -0800

    Jumbotron: text styles

--- src/components/Home/Jumbotron.vue
+++ src/components/Home/Jumbotron.vue
@@ -5,7 +5,12 @@
       alt=""
       class="jumbotron__image"
     />
-    <div class="home-jumbotron__text"></div>
+    <div class="home-jumbotron__container">
+      <h1 class="home-jumbotron__title">CSULB Tzu Ching</h1>
+      <p class="home-jumbotron__description">
+        "With open arms and helping hands, our volunteers are here to serve you"
+      </p>
+    </div>
   </div>
 </template>
 
@@ -18,20 +23,43 @@ export default {};
   height: 100vh;
   position: relative;
   text-align: center;
-  color: white;
 }
-.home-jumbotron__text {
+.home-jumbotron__container {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 }
+.home-jumbotron__title,
+.home-jumbotron__description {
+  color: white;
+}
 
+.home-jumbotron__description {
+  font-size: 1.6rem;
+}
 .jumbotron__image {
   object-fit: cover;
   width: 100%;
   position: relative;
   height: 110vh;
   z-index: 0;
+}
+
+@media (min-width: 0px) {
+  .home-jumbotron__title {
+    font-size: 15vw;
+  }
+}
+@media (min-width: 576px) {
+  .home-jumbotron__title {
+    font-size: 10vw;
+  }
+}
+
+@media (min-width: 992px) {
+  .home-jumbotron__title {
+    font-size: 5rem;
+  }
 }
 </style>
