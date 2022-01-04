28f9491db0042f027d65b85c0f611bd9ad46f825
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-04 12:35:06 -0800

    Jumbotron: styles

--- src/components/Home/Jumbotron.vue
+++ src/components/Home/Jumbotron.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="jumbotron">Test</div>
+  <div class="home-jumbotron">
+    <img
+      src="https://res.cloudinary.com/buraiyen/image/upload/b_black,o_70/v1636913492/CSULB_TC_Website/17097614_645084968996981_6289676080599741977_o_sewcrm.jpg"
+      alt=""
+      class="jumbotron__image"
+    />
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.home-jumbotron {
+  height: 100vh;
+}
+.jumbotron__image {
+  object-fit: cover;
+  width: 100%;
+  height: 120vh;
+}
+</style>
