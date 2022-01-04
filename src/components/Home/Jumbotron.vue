f64b3938f1ad321fc70605e9f756e645397f2c0f
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-04 13:14:20 -0800

    Jumbotron: changed text positioning

--- src/components/Home/Jumbotron.vue
+++ src/components/Home/Jumbotron.vue
@@ -28,7 +28,7 @@ export default {};
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -70%);
 }
 .home-jumbotron__title,
 .home-jumbotron__description {
