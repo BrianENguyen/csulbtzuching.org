06a29c01b2ffe9b074e027df8d04dbe33a613b61
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-04 13:03:24 -0800

    Jumbotron: styles

--- src/components/Home/Jumbotron.vue
+++ src/components/Home/Jumbotron.vue
@@ -5,6 +5,7 @@
       alt=""
       class="jumbotron__image"
     />
+    <div class="home-jumbotron__text"></div>
   </div>
 </template>
 
@@ -15,10 +16,22 @@ export default {};
 <style>
 .home-jumbotron {
   height: 100vh;
+  position: relative;
+  text-align: center;
+  color: white;
 }
+.home-jumbotron__text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
 .jumbotron__image {
   object-fit: cover;
   width: 100%;
-  height: 120vh;
+  position: relative;
+  height: 110vh;
+  z-index: 0;
 }
 </style>
