5a8124a76c9fd3c6b6886474d8aa552c653ff03d
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-04 11:13:47 -0800

    changed fade out animation

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <transition
         mode="out-in"
         enter-active-class="animate__animated animate__fadeIn"
-        leave-active-class="animate__animated animate__fadeOutUp"
+        leave-active-class="animate__animated animate__fadeOut"
       >
         <router-view />
       </transition>
