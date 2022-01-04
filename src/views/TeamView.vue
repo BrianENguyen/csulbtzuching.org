2811dfe70b939cff4f23479f9dcf29296ce5990c
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-04 11:00:56 -0800

    TeamView: implemented team data

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -39,6 +39,7 @@ import TeamMemberCard from '../components/Team/TeamMemberCard';
 import CardWrapper from '../components/UI/CardWrapper';
 import PageTitle from '../components/UI/Header/PageTitle';
 import PageHeader from '../components/UI/Header/PageHeader';
+import { officers, alumnis } from '../components/Helpers/TeamData';
 
 export default {
   components: {
@@ -48,101 +49,8 @@ export default {
     PageHeader,
   },
   data: () => ({
-    officers: [
-      {
-        name: 'Samanta C.',
-        role: 'President',
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/v1638506409/CSULB_TC_Website/team_pic/sam_qvxb4x.jpg',
-        description:
-          'Plans monthly events, leads general meetings, delegates tasks to other officers',
-        links: {
-          email: 'mailto:samanta.Corte@student.csulb.edu',
-          linkedin: 'https://www.linkedin.com/in/samanta-corte-2000',
-          instagram: 'https://www.instagram.com/samantacorte/',
-          food: 'https://www.instagram.com/forevercherriesontop/',
-        },
-      },
-      {
-        name: 'Vivienne A.',
-        role: 'Executive Vice President',
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/v1638054519/CSULB_TC_Website/team_pic/vivienne.jpg',
-        description: 'Helps lead organization with president',
-        links: {
-          email: 'mailto:Vivienne.An@student.csulb.edu',
-          facebook: 'https://www.facebook.com/profile.php?id=100006438256523',
-        },
-      },
-      {
-        name: 'Thu N.',
-        role: 'VP of Administration',
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1236/v1638065296/CSULB_TC_Website/team_pic/thu.jpg',
-        description:
-          "Manages files on organization's online drive, tracks members' volunteering hours",
-        links: {
-          email: 'mailto:nhanhthu920@gmail.com',
-          linkedin: 'https://www.linkedin.com/in/thu-h-nguyen2/',
-          facebook: 'https://www.facebook.com/profile.php?id=100045753340354',
-          instagram: 'https://www.instagram.com/thu.n02/',
-        },
-      },
-      {
-        name: 'Jose A.',
-        role: 'Treasurer',
-        description: 'Handles all financial and budgeting of the organization',
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/v1638071353/CSULB_TC_Website/team_pic/jose.jpg',
-        links: {
-          email: 'mailto:josecarlos.alcantara2016@gmail.com',
-          linkedin: 'https://www.linkedin.com/in/jose-alcantara-321378188',
-          instagram: 'https://www.instagram.com/jose.alcantara22/',
-          art: 'https://www.instagram.com/art.jose22/',
-        },
-      },
-      {
-        name: 'Mahya J.',
-        role: 'VP of Communications',
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/v1638067243/CSULB_TC_Website/team_pic/mahya.jpg',
-        description:
-          'Sends monthly emails that advertise general meetings and volunteering events',
-        links: {
-          email: 'mailto:Habibimasar99@gmail.com',
-          linkedin: 'https://www.linkedin.com/in/mahya-jalali-42425561/',
-          instagram: 'https://www.instagram.com/missshadesofcool/',
-          tiktok: 'https://www.tiktok.com/@fragileseams',
-        },
-      },
-    ],
-    alumnis: [
-      {
-        name: 'Brian N.',
-        role: 'Webmaster / Photographer',
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/e_brightness:27/v1638230670/CSULB_TC_Website/team_pic/brian2.jpg',
-        description:
-          'Manages CSULB TC website and provides photography at events',
-        links: {
-          email: 'mailto:brian.edison.nguyen@gmail.com',
-          linkedin: 'https://www.linkedin.com/in/brian-edison-nguyen/',
-          site: 'https://brianenguyen.com',
-        },
-      },
-      {
-        name: 'Lily K.',
-        role: 'Graphic Designer',
-        description:
-          "Designed our organization's logo, provides art/designs for social media posts",
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/v1638064513/CSULB_TC_Website/team_pic/lily.jpg',
-        links: {
-          site: 'https://www.lilykang.com/',
-          instagram: 'https://www.instagram.com/lilykang.design/',
-        },
-      },
-      {
-        name: 'Weili K.',
-        role: 'Advisor',
-        description:
-          'Advises officer board, reports activities to the Tzu Chi Foundation, supports CSULB TC with volunteering opportunities, hosts monthly book club event',
-        src: 'https://res.cloudinary.com/buraiyen/image/upload/v1638304013/CSULB_TC_Website/team_pic/weili.jpg',
-      },
-    ],
+    officers: officers,
+    alumnis: alumnis,
   }),
 };
 </script>
