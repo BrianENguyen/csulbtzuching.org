150eb7d576f5fe9d15ff4b3e2e7517c731c83bf1
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-23 10:32:06 -0800

    updated Quote route

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,7 @@
 import Jumbotron from '../components/UI/Jumbotron';
 import Introduction from '../components/Introduction';
 import UpcomingEvents from '../components/Events/Upcoming/UpcomingEvents';
-import Quote from '../components/Quote';
+import Quote from '../components/Home/Quote';
 import RecentPhotos from '../components/Photos/RecentPhotos';
 import CardWrapper from '../components/UI/CardWrapper.vue';
 
