86ba58084593e9b6cf520ba66ffabef75b0a1b5b
buraiyen <brian.edison.nguyen@gmail.com>
2022-03-07 12:04:58 -0800

    Introduction: removed row/col, added comments

--- src/components/Home/Introduction.vue
+++ src/components/Home/Introduction.vue
@@ -1,26 +1,23 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../../assets/logo.png')"
-          :lazy-src="require('../../assets/logo.png')"
-          class="my-3"
-          contain
-          height="300"
-          data-aos="zoom-in-down"
-          data-aos-duration="500"
-        />
-        <PageTitle>Welcome to CSULB Tzu Ching!</PageTitle>
-        <p class="intro_description">
-          CSULB Tzu Ching (also known as the Tzu Chi Collegiate Association of
-          CSULB) are a volunteering organization of diverse students that
-          focuses on medicine, education, charity, and humanistic culture. We
-          serve as the CSULB collegiate chapter for our mother organization: the
-          Tzu Chi Foundation.
-        </p>
-      </v-col>
-    </v-row>
+    <!-- CSULB Tzu Ching Logo -->
+    <v-img
+      :src="require('../../assets/logo.png')"
+      :lazy-src="require('../../assets/logo.png')"
+      class="my-3"
+      contain
+      height="300"
+      data-aos="zoom-in-down"
+      data-aos-duration="500"
+    />
+    <PageTitle>Welcome to CSULB Tzu Ching!</PageTitle>
+    <p class="intro_description">
+      CSULB Tzu Ching (also known as the Tzu Chi Collegiate Association of
+      CSULB) are a volunteering organization of diverse students that focuses on
+      medicine, education, charity, and humanistic culture. We serve as the
+      CSULB collegiate chapter for our mother organization: the Tzu Chi
+      Foundation.
+    </p>
   </v-container>
 </template>
 
