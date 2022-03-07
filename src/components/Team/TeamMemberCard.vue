422215df4f462d821a1301127fa95e2c3d5d2996
buraiyen <brian.edison.nguyen@gmail.com>
2022-03-07 12:26:24 -0800

    TeamMemberCard: comments

--- src/components/Team/TeamMemberCard.vue
+++ src/components/Team/TeamMemberCard.vue
@@ -1,10 +1,15 @@
 <template>
   <v-card outlined elevation="5" data-aos="fade-up" data-aos-delay="400">
     <div class="card_content">
+      <!-- Team member image -->
       <TeamMemberImage :src="member.src" />
+      <!-- Team member name -->
       <h3 class="member_name">{{ member.name }}</h3>
+      <!-- Team member role (i.e. president, treasurer, etc) -->
       <p>{{ member.role }}</p>
+      <!-- Team member's role description -->
       <p class="member_description">{{ member.description }}</p>
+      <!-- External links to team member's socials / websites, if any -->
       <div v-if="member.links">
         <TeamMemberLinks :links="member.links" />
       </div>
