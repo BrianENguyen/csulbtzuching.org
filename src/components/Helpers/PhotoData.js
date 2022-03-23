39ac83190321c5b02335ad681fc8a9c32992e8fb
buraiyen <brian.edison.nguyen@gmail.com>
2022-03-23 14:06:15 -0700

    PhotoData: JPG to WebP

--- src/components/Helpers/PhotoData.js
+++ src/components/Helpers/PhotoData.js
@@ -1,22 +1,26 @@
+/* 
+PhotoData is used for the photo gallery in the Photos view. 
+Be sure to order events chronologically in ascending order
+*/
 const PhotoData = [
   {
     name: 'Medical Outreach',
     date: '11/13/21',
     photos: [
-      'https://res.cloudinary.com/buraiyen/image/upload/v1637183817/CSULB_TC_Website/6F3E6219-7561-4D01-AA4A-B13A735CCE80_kjncp4.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/v1637183822/CSULB_TC_Website/IMG_9996_bdgkjf.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/v1638232325/CSULB_TC_Website/outreach_totlvn.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1648069419/CSULB_TC_Website/Gallery/2021-11-13_MedicalOutreach/IMG_9996_bdgkjf_yvg6j0.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1648069429/CSULB_TC_Website/Gallery/2021-11-13_MedicalOutreach/outreach_totlvn_sznera.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1648069430/CSULB_TC_Website/Gallery/2021-11-13_MedicalOutreach/outreach1_k1nzkx.webp',
     ],
   },
   {
     name: 'Fall Week of Welcome',
     date: '9/5/21',
     photos: [
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1637269283/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1548_pynqen.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1084/v1637269278/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1632_sdahq2.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1148/v1637269268/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1689_fjaew9.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_892/v1637269276/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1533_uplruz.jpg',
-      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1020/v1637269282/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1661_klychz.jpg',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1637269283/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1548_pynqe.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1648068794/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1632_sdahq.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1148/v1637269268/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1689_fjaew.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_892/v1637269276/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1533_uplru.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1020/v1637269282/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1661_klych.webp',
     ],
   },
 ];
