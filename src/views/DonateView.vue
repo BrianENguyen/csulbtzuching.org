e04b9a0a1ed185bff623328243ae5cc5c2bdfb60
buraiyen <brian.edison.nguyen@gmail.com>
2022-03-23 11:48:44 -0700

    DonateView: replaced "link" with "button"

--- src/views/DonateView.vue
+++ src/views/DonateView.vue
@@ -4,7 +4,7 @@
     <p class="my-3">
       Donations are greatly appreciated because they help keep the website
       online! If you want to support our organization, then please consider
-      donating! The link below will redirect you to our web developer's Venmo
+      donating! The button below will redirect you to our web developer's Venmo
       page where you can make your donation. Alternatively, you could look up
       the username @Buraiyen on Venmo.
     </p>
@@ -12,7 +12,7 @@
     <p class="my-3">
       You can also donate to our mother organization, the Tzu Chi Foundation, on
       their website. Your contribution will help Tzu Chi with their efforts in
-      providing relief, like food, clothing, and medical care! The link below
+      providing relief, like food, clothing, and medical care! The button below
       will redirect you to the Tzu Chi Foundation's donation page. From there,
       you can choose you much you would like to donate and what your donation
       can be used for, like charity, disaster relief, etc.
