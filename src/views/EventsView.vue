b5260df4edf6a4adfb5c8047ffc9c6ef0fddcc17
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-24 11:02:06 -0700

    EventData: may 2022 events
    Some events are missing information. Will provided them in the future

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -12,6 +12,7 @@
       ></v-checkbox>
       <!-- Uncondensed timeline -->
       <div v-if="!isCondensed">
+        <EventTimeline month="May 2022" :events="may" />
         <EventTimeline month="April 2022" :events="april" />
         <EventTimeline month="March 2022" :events="march" />
         <EventTimeline month="December 2021" :events="december" />
@@ -34,6 +35,7 @@ import EventTimeline from '../components/Events/Regular/EventTimeline';
 import EventTimelineCondensed from '../components/Events/Condensed/EventTimelineCondensed';
 import CardWrapper from '../components/UI/CardWrapper';
 import {
+  may,
   april,
   march,
   december,
@@ -54,6 +56,7 @@ export default {
   },
   data: () => ({
     isCondensed: false,
+    may: may,
     april: april,
     march: march,
     december: december,
