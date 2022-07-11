e87849d28a0530bfa4cc9ee37007e467a409786a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-10 20:32:28 -0700

    PhotoGallery: Spring WoW 2020

--- src/components/Helpers/PhotoData.js
+++ src/components/Helpers/PhotoData.js
@@ -5,7 +5,7 @@ Be sure to order events chronologically in ascending order
 const PhotoData = [
   {
     name: 'Medical Outreach',
-    date: '11/13/21',
+    date: 'November 2021',
     photos: [
       'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1648069419/CSULB_TC_Website/Gallery/2021-11-13_MedicalOutreach/IMG_9996_bdgkjf_yvg6j0.webp',
       'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1648069429/CSULB_TC_Website/Gallery/2021-11-13_MedicalOutreach/outreach_totlvn_sznera.webp',
@@ -14,13 +14,22 @@ const PhotoData = [
   },
   {
     name: 'Fall Week of Welcome',
-    date: '9/5/21',
+    date: 'September 2021',
     photos: [
       'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1637269283/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1548_pynqe.webp',
       'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1151/v1648068794/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1632_sdahq.webp',
       'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1148/v1637269268/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1689_fjaew.webp',
       'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_892/v1637269276/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1533_uplru.webp',
       'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1020/v1637269282/CSULB_TC_Website/Gallery/2021_WeekOfWelcome/BEN_1661_klych.webp',
+    ],
+  },
+  {
+    name: 'Spring Week of Welcome',
+    date: 'February 2020',
+    photos: [
+      'https://res.cloudinary.com/buraiyen/image/upload/v1657509907/CSULB_TC_Website/Gallery/2020_SpringWoW/DSC_0234_tmd218.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1657509906/CSULB_TC_Website/Gallery/2020_SpringWoW/DSC_0219_mrxqup.webp',
+      'https://res.cloudinary.com/buraiyen/image/upload/v1657509905/CSULB_TC_Website/Gallery/2020_SpringWoW/DSC_0210_rbjfqx.webp',
     ],
   },
 ];
