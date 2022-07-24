f5de590cc7a52b6681e4f99b60c79977d127be8f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-24 16:09:17 -0700

    main.js: removed IconsPlugin from bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { BootstrapVue } from 'bootstrap-vue';
 import 'animate.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -13,7 +13,6 @@ import '@/assets/app.css';
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
 
 new Vue({
   router,
