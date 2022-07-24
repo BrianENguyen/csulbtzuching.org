83ba4d2c2b09db62fa91f8626c06554ff09ebe90
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-24 14:07:23 -0700

    PhotosView: small description

--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -2,6 +2,13 @@
   <div class="photos">
     <Jumbotron :jumboData="jumboData" />
     <CardWrapper>
+      <p style="font-size: 1.2rem">
+        Below are our photos from our recent events.
+        <a href="https://flickr.com/people/135720189@N02/" target="_blank"
+          >Click here</a
+        >
+        to view photos from previous years!
+      </p>
       <div v-for="(event, i) in events" :key="i">
         <PhotoGallery :event="event" />
       </div>
@@ -18,7 +25,6 @@ import PhotoData from '../components/Helpers/PhotoData.js';
 const jumboImage =
   'https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1915/v1648148277/CSULB_TC_Website/Jumbotron/BEN_tc-keychain.webp';
 const jumboTitle = 'Photos';
-const jumboDescription = 'Photos from our recent events!';
 
 export default {
   components: {
@@ -31,7 +37,6 @@ export default {
     jumboData: {
       src: jumboImage,
       title: jumboTitle,
-      description: jumboDescription,
     },
   }),
 };
