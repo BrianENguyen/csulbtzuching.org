c1d86d68e93154421e8a756c3cd4741380d99456
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-26 11:22:05 -0700

    init AnimateFadeIn

---
+++ src/components/Animations/AnimateFadeIn.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-lazy
+    :options="{
+      threshold: 0.9,
+    }"
+    transition="fade-transition"
+  >
+    <slot></slot>
+  </v-lazy>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
