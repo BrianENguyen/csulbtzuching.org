c58d334697e4e60c94c7d6925d3aaf1a71334f7f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-28 11:44:08 -0700

    Footer: lazy load

--- src/components/UI/Footer.vue
+++ src/components/UI/Footer.vue
@@ -1,34 +1,42 @@
 <template>
-  <v-footer color="white" id="footer" padless>
-    <v-row id="footer_content" no-gutters>
-      <v-col cols="12">
-        <v-card flat tile
-          ><a
-            v-for="externalLink in externalLinks"
-            :key="externalLink.icon"
-            :href="externalLink.hyperlink"
-            target="_blank"
-          >
-            <img
-              :src="externalLink.icon"
-              class="mx-3 icon-link"
-              :alt="externalLink.alt"
-            /> </a
-        ></v-card>
-        <v-col class="py-4" cols="12">
-          <p>{{ new Date().getFullYear() }} — CSULB Tzu Ching</p>
-          <p>
-            Designed and developed by
-            <a href="https://brianenguyen.com/" target="_blank">Brian Nguyen</a>
-          </p>
+  <AnimateFadeIn>
+    <v-footer color="white" id="footer" padless>
+      <v-row id="footer_content" no-gutters>
+        <v-col cols="12">
+          <v-card flat tile
+            ><a
+              v-for="externalLink in externalLinks"
+              :key="externalLink.icon"
+              :href="externalLink.hyperlink"
+              target="_blank"
+            >
+              <img
+                :src="externalLink.icon"
+                class="mx-3 icon-link"
+                :alt="externalLink.alt"
+              /> </a
+          ></v-card>
+          <v-col class="py-4" cols="12">
+            <p>{{ new Date().getFullYear() }} — CSULB Tzu Ching</p>
+            <p>
+              Designed and developed by
+              <a href="https://brianenguyen.com/" target="_blank"
+                >Brian Nguyen</a
+              >
+            </p>
+          </v-col>
         </v-col>
-      </v-col>
-    </v-row>
-  </v-footer>
+      </v-row>
+    </v-footer>
+  </AnimateFadeIn>
 </template>
 
 <script>
+import AnimateFadeIn from '../Animations/AnimateFadeIn';
 export default {
+  components: {
+    AnimateFadeIn,
+  },
   data() {
     return {
       // you need the 'require' function when binding images
