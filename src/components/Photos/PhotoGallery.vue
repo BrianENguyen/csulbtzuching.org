a2a5b9ff0055c39baa52ea07711f93bfca4d8a50
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-28 11:56:03 -0700

    PhotoGallery: lazy loaded rows

--- src/components/Photos/PhotoGallery.vue
+++ src/components/Photos/PhotoGallery.vue
@@ -7,26 +7,31 @@
     >
       {{ event.name }}, {{ event.date }}
     </h2>
-    <v-row>
-      <v-col
-        v-for="photo in event.photos"
-        :key="photo"
-        cols="6"
-        lg="4"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      >
-        <PhotoCard :photo="photo" />
-      </v-col>
-    </v-row>
+    <AnimateFadeIn>
+      <v-row>
+        <v-col
+          v-for="photo in event.photos"
+          :key="photo"
+          cols="6"
+          lg="4"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <PhotoCard :photo="photo" />
+        </v-col>
+      </v-row>
+    </AnimateFadeIn>
   </div>
 </template>
 
 <script>
 import PhotoCard from './PhotoCard.vue';
+import AnimateFadeIn from '../Animations/AnimateFadeIn';
+
 export default {
   components: {
     PhotoCard,
+    AnimateFadeIn,
   },
   props: {
     event: Object,
