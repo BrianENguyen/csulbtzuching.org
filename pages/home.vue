267c25a4a01d9dff21b1161167bd92e69fdf9222
buraiyen <brian.edison.nguyen@gmail.com>
2023-06-29 11:24:52 -0700

    :sparkles: init: home page

---
+++ pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <section
+    class="
+      h-screen
+      bg-gradient-to-r 
+    from-gray-700 
+    to-gray-900
+      grid
+      grid-rows-2
+      text-white
+      "
+  >
+    <div class="container mx-auto">
+      <h1 class="text-5xl">Weili Kang</h1>
+      <p>Pianist, flutist, music teacher, private tutorer</p>
+    </div>
+  </section>
+</template>
