4cde5641720edd5cc7b7cd36f66323857bf8c769
buraiyen <brian.edison.nguyen@gmail.com>
2023-07-09 09:07:38 -0700

    :sparkles: feat: index ui changes
    - intro section
    - web card styles

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,63 @@
       grid
       grid-rows-2
       text-white
+      mb-4
       "
   >
     <div class="container mx-auto">
       <h1 class="text-5xl">Weili Kang</h1>
       <p>Pianist, flutist, music teacher, private tutorer</p>
+    </div>
+  </section>
+
+  <section class="container mx-auto grid grid-cols-2 gap-4">
+    <div>
+      <h2 class="text-4xl text-center mb-4">Welcome to Weili's Website</h2>
+      <p class="mb-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere corporis
+        doloremque iste quisquam est, sed laudantium numquam optio sequi
+        eligendi natus facilis odio provident distinctio exercitationem quis
+        voluptatibus nesciunt quas.
+      </p>
+      <p class="mb-5">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        obcaecati ad, eligendi inventore facere illo non recusandae at
+        dignissimos
+      </p>
+      <p class="mb-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
+        perspiciatis nemo numquam quos ipsa illum est dicta eos magni amet aut
+        neque, voluptates et! Error laudantium natus quae perspiciatis placeat
+        omnis asperiores amet explicabo. Error, minima ducimus, tenetur ipsa
+        veniam et saepe officiis nesciunt quidem illo tempore vero iure odio!
+      </p>
+    </div>
+    <div>
+      <h3>Insert Image here</h3>
+    </div>
+  </section>
+
+  <section
+    class="
+      bg-gradient-to-r 
+    from-gray-700 
+    to-gray-900
+    grid
+    grid-cols-3
+    gap-4
+    py-6
+  "
+  >
+    <div
+      class="text-white rounded-md bg-gray-900 border-l-slate-500 border-l-4 w-[400px] h-[150px] mx-auto"
+    >
+      <div class="mt-4 ml-4">
+        <h3 class="text-2xl font-bold">About</h3>
+        <p class="text-lg mb-5">Get to know about me and my career</p>
+        <a class="text-blue-300 cursor-pointer font-bold italic mt-auto"
+          >Learn More</a
+        >
+      </div>
     </div>
   </section>
 </template>
