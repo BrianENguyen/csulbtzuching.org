5e60d28eba7cef4448a7abbb301ed320fb0c0d91
buraiyen <brian.edison.nguyen@gmail.com>
2023-07-09 08:39:04 -0700

    :truck: chore: rename home to index

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
-  <Home />
+  <Index />
 </template>
 
 <script>
-import Home from '~/pages/home.vue'
+import Index from '~/pages/index.vue'
 
 export default {
   components: {
-    Home
+    Index
   }
 }
 </script>
