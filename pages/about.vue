8750573de84eb6c03fa6c91998f8a43d2182d04a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-07-16 10:19:41 -0700

    feat: about page content

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>About</h1>
+  <main>
+    <section class="container mx-auto grid grid-cols-2">
+      <div>
+        <h1 class="text-3xl font-bold">About Me</h1>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          doloribus, cupiditate aperiam ullam illo dolores, deserunt, vitae
+          corrupti saepe maxime placeat ea eos magni ex harum quae. Autem
+          nostrum sapiente ipsum quis explicabo tempore ducimus dolorem
+          accusamus quisquam, voluptatibus consectetur illum dolore magnam esse
+          veniam quod quasi id est enim amet. Provident ipsa illum facere itaque
+          omnis a saepe unde rem, nihil id officiis esse atque quae accusantium?
+          Explicabo sint, repellendus est cupiditate commodi harum? Laborum
+          ipsum atque possimus culpa magni voluptate ipsa adipisci voluptatum
+          quis veritatis! Repellendus dignissimos voluptatem quas iste, ut alias
+          tempora, fugiat sed distinctio consequuntur sint!
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus
+          doloremque ipsum quo officia esse repellat incidunt quos placeat,
+          minus dolore illo consequuntur asperiores nostrum, expedita, non
+          fugiat obcaecati? Magnam, totam?
+        </p>
+      </div>
+      <div>
+        <img src="" alt="Image here" />
+      </div>
+    </section>
+  </main>
 </template>
