b13efe33ce09654c1a95093905be6a17453381b4
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-07-26 18:52:39 -0700

    save before switch to unocss

--- app.vue
+++ app.vue
@@ -8,4 +8,5 @@
       text-white
     "
   />
+  <Footer />
 </template>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,16 @@
 <template>
-  <footer class="bg-slate-800">
-    <h2>Weili Kang</h2>
+  <footer class="bg-gray-700 text-white">
+    <section class="container mx-auto">
+      <div class="grid grid-cols-2">
+        <h2>Weili Kang</h2>
+        <ul>
+          <li class="inline">1</li>
+          <li class="inline">1</li>
+          <li class="inline">1</li>
+          <li class="inline">1</li>
+          <li class="inline">1</li>
+        </ul>
+      </div>
+    </section>
   </footer>
 </template>
