422e66a244a670b5201533b2c29427ccfed9170e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-31 22:46:54 -0500

    :sparkles: feat: navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,17 @@
 <template>
-  <nav
-    class="bg-white border-gray-200 dark:bg-gray-900 fixed w-[100%] h-70px z-[1] shadow-xl"
-  >
+  <nav class="bg-white border-gray-200 fixed w-[100%] h-70px z-[1] shadow-xl">
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
       <div class="flex items-center">
-        <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+        <span class="self-center text-2xl font-semibold whitespace-nowrap"
           >CSULB Tzu Ching</span
         >
       </div>
       <button
         data-collapse-toggle="navbar-default"
         type="button"
-        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden"
         aria-controls="navbar-default"
         aria-expanded="false"
         @click="toggleMobileMenu"
@@ -38,7 +35,7 @@
       </button>
       <div class="w-full md:block md:w-auto" id="navbar-default">
         <ul
-          class="list-none rounded shadow-xl font-medium md:flex flex-col p-4 md:p-0 mt-4 border border-gray-100 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="list-none rounded shadow-xl font-medium md:flex flex-col p-4 md:p-0 mt-4 border border-gray-100 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white"
           :class='isMobileMenuOpen ? "flex" : "hidden"'
         >
           <li v-for="link in links" :key="link">
