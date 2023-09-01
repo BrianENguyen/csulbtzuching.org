02d3457652aed96671fe44582f07d7f489884544
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-31 21:13:39 -0500

    :truck: chore: migrated files

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="rounded max-w-[1200px] w-[100%] border-1 border-[#EFEFEF] shadow-2xl border-solid h-auto py-8"
+  >
+    <div class="px-8">
+      <slot />
+    </div>
+  </div>
+</template>
