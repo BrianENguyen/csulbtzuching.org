1cba49585f1125af549be9acf597b076600e2bd0
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-31 22:55:18 -0500

    :speech_balloon: chore: website update text

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,12 +1,19 @@
 <template>
   <section class="mb-24">
-    <h2>Welcome to CSULB Tzu Ching!</h2>
+    <h2 class="text-center">
+      Welcome to CSULB Tzu Ching (and our newly redesigned website!)
+    </h2>
     <p>
       CSULB Tzu Ching (also known as the Tzu Chi Collegiate Association of
       CSULB) are a volunteering organization of diverse students that focuses on
       medicine, education, charity, and humanistic culture. We serve as the
       CSULB collegiate chapter for our mother organization: the Tzu Chi
       Foundation.
+    </p>
+    <p>
+      As of September 2023, this website has been redesigned with a strong focus
+      on simplicity and minimalism while still delivering essential information
+      to our users.
     </p>
   </section>
 </template>
