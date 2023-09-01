418a92bc36f25b02a6ca0d734b1f38293253aad2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-08-31 22:04:40 -0500

    chore: removed unocss nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt'],
+  // modules: ['@unocss/nuxt'],
   components: [{ path: '~/components', extensions: ['.vue'] }],
   css: ['assets/css/global.css', '@unocss/reset/normalize.css'],
 });
